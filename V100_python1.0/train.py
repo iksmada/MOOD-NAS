ea3d770c29ac2332487d016bdaf06500f0029a56
Raphael Adamski <radamski@rxnetworks.com>
2021-05-15 11:20:28 -0300

    Fix log date format

--- V100_python1.0/train.py
+++ V100_python1.0/train.py
@@ -43,7 +43,7 @@ utils.create_exp_dir(args.save, scripts_to_save=None)
 
 log_format = '%(asctime)s %(message)s'
 logging.basicConfig(stream=sys.stdout, level=logging.INFO,
-                    format=log_format, datefmt='%m/%d %I:%M:%S %p')
+                    format=log_format, datefmt='%m/%d %H:%M:%S')
 fh = logging.FileHandler(os.path.join(args.save, 'log.txt'))
 fh.setFormatter(logging.Formatter(log_format))
 logging.getLogger().addHandler(fh)
